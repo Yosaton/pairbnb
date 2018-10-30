4b85b9bc670fc4afe74755145861700fd4907796
dustincrs <dustincrs@outlook.com>
2018-10-30 09:30:37 +0800

    Implement jobs

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -35,7 +35,7 @@ class BookingsController < ApplicationController
 
     respond_to do |format|
       if @booking.save
-        BookingMailer.new_booking_email(@booking).deliver!
+        BookingJob.perform_later(@booking)
         format.html { redirect_to listing_path(@listing), notice: 'Booking was successfully created.' }
         format.json { render :show, status: :created, location: @booking }
       else
