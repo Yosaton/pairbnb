b2f1d685117acf2cdc82f8809bc2c41bbbb3747f
dustincrs <dustincrs@outlook.com>
2018-11-12 18:14:12 +0800

    Wrote migrations for messages, chatrooms, subscriptions

--- db/migrate/20181112101046_create_messages.rb
+++ db/migrate/20181112101046_create_messages.rb
@@ -1,8 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-
-      t.timestamps
+    	t.string 	:content
+    	t.integer	:chatroom_id
+    	t.integer	:user_id
+		t.timestamps
     end
   end
 end
--- db/migrate/20181112101106_create_subscriptions.rb
+++ db/migrate/20181112101106_create_subscriptions.rb
@@ -1,8 +1,9 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
-
-      t.timestamps
+    	t.integer	:user_id
+    	t.integer	:chatroom_id
+		t.timestamps
     end
   end
 end
